de1f03ed0442f459b938c800dbbc1fc0071a596d
Manoj Madavarapu <manojmadavarapu7@gmail.com>
2025-07-16 01:26:02 +0530

    made some ui changes

--- src/Components/PeopleProfile.jsx
+++ src/Components/PeopleProfile.jsx
@@ -7,6 +7,7 @@ import { removeUserFeed } from "../utils/feedSlice";
 const PeopleProfile = () => {
   let [status,setStatus]=useState("");
   let [popUp,setPopUp]=useState(false)
+  let [loading,setIsLoading]=useState(false);
   let location=useLocation();
   let userData=location.state;
   console.log(userData)
@@ -25,6 +26,7 @@ const PeopleProfile = () => {
   }
 
   let handleRequest=async(status,id)=>{
+    setIsLoading(true);
     try{
         let res=await axios.post(`https://devtinder-tjp2.onrender.com/request/${status}/${id}`,{},{withCredentials:true})
         console.log(res.data);
@@ -33,6 +35,9 @@ const PeopleProfile = () => {
     }
     catch(err){
         console.error(err);
+    }
+    finally{
+      setIsLoading(false);
     }
   }
   useEffect(()=>{
@@ -112,9 +117,9 @@ const PeopleProfile = () => {
         {!popUp?
         <>
         <button className="px-6 py-2 bg-geen-800 hover:bg-gren-700 text-white font-semibold rounded-lg shadow-md transition duration-300 bg-amber-600 hover:bg-amber-500 connect_btn" onClick={()=>handleRequest("interested",userData._id)}>
-          Connect
+          {loading?"Please wait...":"Connect"}
         </button>
-        <button className="px-6 py-2 bg-rd-800 hover:bg-gry-300 text-white font-semibold rounded-lg shadow-md transition duration-300 bg-gray-800 hover:bg-gray-700 ignore_btn " onClick={()=>handleRequest("ignored",userData._id)}>
+        <button className="px-6 py-2 bg-rd-800 hover:bg-gry-300 text-white font-semibold rounded-lg shadow-md transition duration-300 bg-gray-800 hover:bg-gray-700 ignore_btn " onClick={()=>handleRequest("ignored",userData._id)} disabled={loading}>
           Ignore
         </button></>:<p className="font-bold -mt-2">Request Send...</p>}
         </>)
