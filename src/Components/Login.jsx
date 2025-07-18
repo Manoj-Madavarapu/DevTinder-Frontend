00ef152340a57d1131eedc107d38a6575dadc165
Manoj Madavarapu <manojmadavarapu7@gmail.com>
2025-07-19 00:19:01 +0530

    made some ui changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -56,23 +56,23 @@ const Login = () => {
       <div className='flex  items-center justify-center mt-20 ' >
         <img src="/images/Collaboration at devTinder Office.png" alt=""  className='hidden md:block w-100 h-100 card_shadow_img'/>
         <div className="card bg-base-300 w-96 h-100  flex flex-col justify-center py-8 card_shadow pl-3">
-              <div className="card-body bg-black !text-white ">
+              <div className="card-body">
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend text-white">Email Id</legend>
-                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="input text-white" placeholder="Enter your Email" />
+                  <legend className="fieldset-legend">Email Id</legend>
+                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="input" placeholder="Enter your Email" />
                   <p className="label"></p>
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend text-white">Password</legend>
+                  <legend className="fieldset-legend">Password</legend>
                   {/* <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} className="input mb-6" placeholder="Enter your Password" /> */}
                   <div className='relative'>
-                  <input type={showPassword?"text":"password"} value={password} onChange={(e) => setPassword(e.target.value)} className="input mb-6 text-white " placeholder="Enter your Password" />
+                  <input type={showPassword?"text":"password"} value={password} onChange={(e) => setPassword(e.target.value)} className="input mb-6 " placeholder="Enter your Password" />
                      <i className={`fa-solid ${showPassword?"fa-eye":"fa-eye-slash"} absolute top-3 text-base -ml-8 cursor-pointer text-gray-400 z-10`} onClick={()=>setShowPaassword((prev)=>!prev)}></i>
                   </div>
                   <p className="label text-red-500 -mt-5 mb-2">{error}</p>
                 </fieldset>
-                <button className="btn font-bold login w-full sm:w-auto text-white" onClick={handleLogin} disabled={loading}>{loading? "Please wait...":"Login"}</button>
-                <p className='mt-2 text-white'>Don't have an Account? <span className='underline font-bold text-blue-500 cursor-pointer' onClick={()=>navigate("/devTinder/sign-up")}> Sign Up</span></p>
+                <button className="btn font-bold login w-full sm:w-auto" onClick={handleLogin} disabled={loading}>{loading? "Please wait...":"Login"}</button>
+                <p className='mt-2 '>Don't have an Account? <span className='underline font-bold text-blue-500 cursor-pointer' onClick={()=>navigate("/devTinder/sign-up")}> Sign Up</span></p>
               </div>
         </div>
       </div>
